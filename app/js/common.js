4b1e84ed569b9b0e19398f3f9a00124ab922ab50
Denys Soloviev <dionisio14solo14@gmail.com>
2019-02-28 17:41:59 +0200

    end project.

--- app/js/common.js
+++ app/js/common.js
@@ -13,7 +13,21 @@ $(function() {
         slidesToShow: 5,
         slidesToScroll: 1,
         speed: 400,
-        autoplay: true
+        autoplay: true,
+        responsive: [
+			{
+			  breakpoint: 992,
+			  settings: {
+				slidesToShow: 3
+			  }
+			},
+			{
+				breakpoint: 576,
+				settings: {
+					slidesToShow: 1
+				}
+			}
+		  ]
     }); 
     
     var $hdr = $('header');
@@ -32,5 +46,55 @@ $(function() {
         }, 700);
     });
     
+    
+    var $btnsModal = $('[data-modal="model"]');
+    $btnsModal.each(function(index) {
+        $(this).click(function(e) {
+            e.preventDefault();
+            $('.overlay-modal').fadeIn(400, function() {
+                    $('.modal-form')
+                        .css('display', 'block')
+                        .animate({
+                            top: 25 + '%',
+                            opacity: 1
+                        });
+            });  
+        });
+    });
+    
+    $('.modal-form .modal-close, .overlay-modal').click(function() {
+        $('.modal-form').animate({
+            opacity: 0,
+            top: 45 + "%"
+        }, 200, function() {
+            $(this).css('display', 'none');
+            $('.overlay-modal').fadeOut(400);
+        }); 
+    });
+    
+    $('.home header nav ul li a').click(function(e) {
+        e.preventDefault();
+        var $href = $(this).attr('href'), $top = $($href).offset().top;
+        
+        $('body, html').animate({
+            scrollTop: $top
+        }, 800);
+    });
+    
+    $('.hamburger').click(function() {
+        $(this).toggleClass('open');
+        $('.popup-menu').css('display', 'flex').animate({
+            left: 0,
+            opacity: 1
+        }, 500);
+    });
+    
+    $('.popup-menu .close, .popup-menu ul li a').click(function() {
+         $(this).closest('.popup-menu').css('display', 'none').animate({
+            left: -135 + "px",
+            opacity: 0
+         }, 300);
+         $('.hamburger').removeClass('open');   
+    });
     
 });
